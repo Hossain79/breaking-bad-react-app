8f7c8551d3674e5591cfd7176e9ff09af05dcde2
Hossain79 <hossainsharif79@gmail.com>
2020-10-15 09:42:09 +0200

    Updates

---
+++ precache-manifest.28aac0038701f582a37e48edd2afdf61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1745351fab17fa9ee96071fdb394e3c8",
+    "url": "/breaking-bad-react-app/index.html"
+  },
+  {
+    "revision": "1e3810bfd0aa31bffa74",
+    "url": "/breaking-bad-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8d881336a0381bdfaaff",
+    "url": "/breaking-bad-react-app/static/js/2.004e67ac.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/breaking-bad-react-app/static/js/2.004e67ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e3810bfd0aa31bffa74",
+    "url": "/breaking-bad-react-app/static/js/main.b47b2781.chunk.js"
+  },
+  {
+    "revision": "95aecc04baba64494f32",
+    "url": "/breaking-bad-react-app/static/js/runtime-main.5e7aca39.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/breaking-bad-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
